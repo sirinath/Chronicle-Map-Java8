bb30370fd5aa7c539e03d4fa164ccd5ef3596ac2
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2014-11-30 14:19:40 +0200

    HCOLL-211 Add BytesMarshaller and improve handling of invalid sizes.

---
+++ src/test/java/net/openhft/chronicle/map/JavaBeanInterface.java
@@ -0,0 +1,56 @@
+package net.openhft.chronicle.map;
+
+import net.openhft.lang.model.constraints.MaxSize;
+
+public interface JavaBeanInterface {
+    void setFlag(boolean flag);
+
+    boolean getFlag();
+
+    void setByte(byte b);
+
+    byte getByte();
+
+
+    void setShort(short s);
+
+    short getShort();
+
+
+    void setChar(char ch);
+
+    char getChar();
+
+
+    void setInt(int i);
+
+    int getVolatileInt();
+
+    void setOrderedInt(int i);
+
+    int getInt();
+
+
+    void setFloat(float f);
+
+    float getFloat();
+
+
+    void setX(long l);
+
+    long getX();
+
+    long addX(long toAdd);
+
+    void setDouble(double d);
+
+    double getDouble();
+
+    double addAtomicDouble(double toAdd);
+
+    void setString(@MaxSize(8) String s);
+
+    String getString();
+
+    StringBuilder getUsingString(StringBuilder b);
+}
---
+++ src/test/java/net/openhft/chronicle/map/PingPongMapMain.java
@@ -0,0 +1,43 @@
+package net.openhft.chronicle.map;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PingPongMapMain {
+    public static void main(String... ignored) throws IOException {
+        File file = new File("shared");
+        int keys = 10_000_000;
+        try (ChronicleMap<String, MyLong> map = ChronicleMapBuilder
+                .of(String.class, MyLong.class)
+                .entrySize(32)
+                .entries(keys)
+                .create()) {
+
+//            LongValue lv = DataValueClasses.newDirectReference(LongValue.class);
+            long start = System.currentTimeMillis();
+            long value = 0;
+            Mutator<MyLong, Long> mutator = v -> v.addValue(1);
+            for (int j = 0; j < keys; j++) {
+                String key = "hello-" + j;
+//                for (int i = 0; i < 10; i++) {
+                value = map.updateForKey(key, mutator);
+//                }
+            }
+            long time = System.currentTimeMillis() - start;
+            System.out.println("value=" + value + " took " + time + " ms");
+/*
+            try (WriteContext wc = map.acquireUsingLocked("hello", lv)) {
+                value = lv.addValue(1);
+            }
+*/
+        }
+    }
+}
+
+interface MyLong {
+    public long getValue();
+
+    public void setValue(long l);
+
+    public long addValue(long l);
+}
